692d510990f8bcedff6c9fa50b13ae4c8eba1093
Usein Abilev <the.usein@gmail.com>
2024-10-27 09:50:14 +0100

    feat: add translation service

---
+++ src/api/services/translation.service.ts
@@ -0,0 +1,92 @@
+import appConfig from "../../appConfig";
+import Debug from "../decorators/debug.decorator";
+
+type TranslateLanguageCode = "en" | "ru";
+
+export interface TranslateOptions {
+    source: TranslateLanguageCode | "auto";
+    target: TranslateLanguageCode;
+    format: "text";
+    alternatives?: number;
+}
+
+type TranslateResultData = {
+    translatedText: string;
+    alternatives: string[];
+    detectedLanguage: { confidence: number; language: TranslateLanguageCode };
+};
+
+type DetectResultData = {
+    confidence: number;
+    language: TranslateLanguageCode;
+}[];
+
+type APIPath = "translate" | "detect";
+
+interface TranslateDefaultErrorObject {
+    status: number;
+    message: string;
+}
+
+interface TranslateServiceSuccess<T> {
+    ok: true;
+    data: T;
+}
+interface TranslateServiceError<T> {
+    ok: false;
+    error: T;
+}
+type TranslateServiceResponse<TSuccess, TError = TranslateDefaultErrorObject> =
+    | TranslateServiceSuccess<TSuccess>
+    | TranslateServiceError<TError>;
+
+export default class TranslationService {
+    constructor() {}
+
+    @Debug({ name: "TranslationService.detect" })
+    public async detect(text: string): Promise<TranslateServiceResponse<DetectResultData>> {
+        return this.fetch("detect", { q: text });
+    }
+
+    @Debug({ name: "TranslationService.translate" })
+    public async translate(
+        text: string,
+        options: TranslateOptions = {
+            source: "auto",
+            target: "en",
+            format: "text",
+            alternatives: 3,
+        },
+    ): Promise<TranslateServiceResponse<TranslateResultData>> {
+        return this.fetch("translate", {
+            q: text,
+            ...options,
+        });
+    }
+
+    private async fetch<TSuccess, TError = TranslateDefaultErrorObject>(
+        path: APIPath,
+        params: Record<string, unknown> = {},
+    ): Promise<TranslateServiceResponse<TSuccess, TError>> {
+        const url = `http://${appConfig.translation.host}:${appConfig.translation.port}/${path}`;
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(params),
+            headers: { "Content-Type": "application/json" },
+        })
+            .then((res) => res.json())
+            .catch((error) => {
+                return {
+                    ok: false,
+                    error: {
+                        status: error.status || 500,
+                        message: error.message || error.error,
+                    },
+                };
+            });
+        return {
+            ok: true,
+            data: response,
+        };
+    }
+}
