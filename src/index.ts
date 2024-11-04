3e3cc63e6b2e23b86c18d66a1ee9eaaa6a06ab62
Usein Abilev <the.usein@gmail.com>
2024-11-04 23:18:41 +0100

    refactor: simplify database initialization by using a dedicated function

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,11 @@
 import "reflect-metadata";
-import { DataSource } from "typeorm";
-import appConfig from "./appConfig";
+
 import LanguageBotAPI from "./api";
 import initializeTgBot from "./bot";
-import UserEntity from "./api/db/entities/user.entity";
-import UserCardEntity from "./api/db/entities/card.entity";
-import { logger } from "./utils/logger.util";
+import initializeDatabase from "./api/db";
 
 (async () => {
-    const dataSource = new DataSource({
-        type: "postgres",
-        poolSize: 10,
-        host: appConfig.database.host,
-        port: appConfig.database.port,
-        database: appConfig.database.name,
-        username: appConfig.database.username,
-        password: appConfig.database.password,
-        entities: [UserEntity, UserCardEntity],
-        synchronize: true,
-    });
-    while (!dataSource.isInitialized) {
-        await dataSource.initialize().catch((error) => {
-            console.error("Failed to initialize database connection:", error);
-        });
-        await new Promise((resolve) => setTimeout(resolve, 3000));
-    }
-    logger.info("Database connected successfully", appConfig.database);
+    const dataSource = await initializeDatabase();
     const api = new LanguageBotAPI(dataSource);
     await initializeTgBot(api);
 })();
