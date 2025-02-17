ab9f0f99c2cf75f7972aad883050d19de7c0e6a8
Usein Abilev <the.usein@gmail.com>
2025-02-17 16:16:15 +0100

    feat(web, home): implement activity blocks and enhance user greeting section

--- apps/web/src/app/screens/Home.tsx
+++ apps/web/src/app/screens/Home.tsx
@@ -1,33 +1,37 @@
-import React, { FC, useEffect } from "react";
+import React, { useCallback, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { Button, List, Section } from "@telegram-apps/telegram-ui";
+import { List } from "@telegram-apps/telegram-ui";
 import { useQuery } from "@tanstack/react-query";
 import { createUserQuery } from "../../features/api/user";
 import { ROUTES } from "../../constants/routes";
 import { DeckListBlock } from "../../components/deck";
 import { DeckSchema } from "../../features/types/deck.types";
-import SectionHeader from "../../components/SectionHeader/SectionHeader";
+import SectionHeader from "../../components/Section/SectionHeader";
 import SVGIcon from "../../components/icons/SVGIcon";
+import ActivityBlock, { ActivityBlockProps } from "../../components/ActivityBlock/ActivityBlock";
+import Section from "../../components/Section/Section";
 
 const ListStyled = styled(List)`
     padding: 0;
 
-    .greeting-title {
-        font-size: 32px;
-        line-height: 35px;
-        font-weight: 500;
-        padding: 24px 0 0 28px;
-    }
-`;
+    .home-header {
+        .greeting-title {
+            padding: 24px 28px 0 28px;
+            font-size: 32px;
+            line-height: 35px;
+            font-weight: 500;
+            color: var(--app-title-text-color);
+        }
 
-const DeckSectionStyled = styled.div`
-    padding: 0 16px;
+        .activity-list {
+            padding: 0 16px;
+            margin-top: 12px;
 
-    .decks-header-content {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
+            display: flex;
+            flex-direction: column;
+            gap: 12px;
+        }
     }
 `;
 
@@ -38,6 +42,40 @@ export default function Home() {
     const navigate = useNavigate();
     const { data: userResult } = useQuery(createUserQuery());
 
+    const openPractice = useCallback(() => {
+        navigate(ROUTES.PRACTICE);
+    }, [navigate]);
+
+    const activities: ActivityBlockProps[] = useMemo(() => {
+        if (!userResult) return [];
+
+        const { practiceCounters } = userResult;
+        if (practiceCounters.cardsToPracticeCount > 0) {
+            if (practiceCounters.cardsToReviewCount === 0) {
+                return [
+                    {
+                        type: "action",
+                        title: "🧠 Test Your Memory!",
+                        description: `New ${practiceCounters.cardsToLearnCount} flashcards are ready for you. See how many you can recall!`,
+                        buttonText: "Start",
+                        onClick: openPractice,
+                    },
+                ];
+            }
+            return [
+                {
+                    type: "action",
+                    title: "🎓 Time to Practice!",
+                    description: `You have ${practiceCounters.cardsToPracticeCount} words waiting for review. Sharpen your memory now!`,
+                    buttonText: "Practice",
+                    onClick: openPractice,
+                },
+            ];
+        }
+
+        return [];
+    }, [userResult, openPractice]);
+
     const handleViewAllDecks = () => {};
 
     const handleDeckClick = (deck: DeckSchema) => {
@@ -46,13 +84,31 @@ export default function Home() {
 
     return (
         <ListStyled>
-            <div className="greeting-title">Hello, {userResult?.user.firstName}!</div>
-            <DeckSectionStyled>
-                <SectionHeader title="My decks" onViewAllClick={handleViewAllDecks} />
-                {userResult?.decks && (
-                    <DeckListBlock decks={userResult.decks} onDeckClick={handleDeckClick} />
+            <header className="home-header">
+                <div className="greeting-title">Hello, {userResult?.user.firstName}!</div>
+                {!!activities.length && (
+                    <div className="activity-list">
+                        {activities.map((activity, index) => (
+                            <ActivityBlock
+                                key={index}
+                                type={activity.type}
+                                title={activity.title}
+                                description={activity.description}
+                                buttonText={activity.buttonText}
+                                onClick={activity.onClick}
+                            />
+                        ))}
+                    </div>
                 )}
-            </DeckSectionStyled>
+            </header>
+            <Section>
+                <SectionHeader title="My decks" onViewAllClick={handleViewAllDecks} />
+                {userResult?.decks?.length ? (
+                    <DeckListBlock decks={userResult.decks} onDeckClick={handleDeckClick} />
+                ) : (
+                    <p>No cards</p>
+                )}
+            </Section>
         </ListStyled>
     );
 }
